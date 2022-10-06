88e99a9e22e912eaf485a93831e566e6514ef740
m2rads <argo.mohrad@gmail.com>
2022-10-05 21:24:25 -0700

    add client side

--- server/src/app.py
+++ server/src/app.py
@@ -1,8 +1,15 @@
 from flask import Flask, make_response
+import pydaisi as pyd
 import os
 import time
 
 app = Flask(__name__)
+
+vehicle_routing_problem_ = pyd.Daisi("soul0101/Vehicle Routing Problem ")
+
+input_locations, vehicle_capacities = []
+
+vehicle_routing_problem_.vrp_calculator(input_locations, vehicle_capacities, carryforward_penalty=1000, search_timeout=10, first_sol_strategy="AUTOMATIC", ls_metaheuristic="AUTOMATIC").value
 
 @app.route('/')
 def index():
