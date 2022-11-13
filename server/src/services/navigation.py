c1ce059c7f3ddab6d18a81d017d8280f27ac940b
Parham Shagerdmootab <psm1379@gmail.com>
2022-11-12 17:06:21 -0800

    Preprocessed and cleaned response obj

--- server/src/services/navigation.py
+++ server/src/services/navigation.py
@@ -4,41 +4,63 @@ import os
 
 class Navigation:
 
-    def __init__(self) -> None:
+    def __init__(self, optimized_routes: dict, addresses:dict) -> None:
         load_dotenv()
         self.gm = googlemaps.Client(os.getenv('GCP_API_Key'))
+        self.optimized_routes = optimized_routes
+        self.addresses = addresses
 
-    def get_direction(self, addresses: list):
+    def get_direction(self, route):
         """Utilizes Google Maps API to get directions for Vehicles
-
-        Args:
-            addresses (list): List of Addresses (First index is the origin)
 
         Returns:
             routes: list
         """
-        origin = addresses.pop(0)
-        routes = self.gm.directions(origin=origin, destination=origin, waypoints=addresses)
+        origin = route.pop(0)
+        routes = self.gm.directions(origin=origin, destination=origin, waypoints=self.addresses)
         return routes
         
-    def find_vehicle_directions(self, optimized_routes: dict, addresses: list):
+    def find_vehicle_directions(self):
         """Finds directions of vehicles based on the optimized route given
         
-        Args:
-            optimized_routes (dict),
-            addresses (list)
         """
         #Separate the vehicles with no destinations
-        for route in optimized_routes:
-            if optimized_routes[route] != [0,0]:
-                for dest in optimized_routes[route]:
+        for route in self.optimized_routes:
+            if self.optimized_routes[route] != [0,0]:
+                for dest in self.optimized_routes[route]:
                     #Put the address based on the index of addresses in oprimized routes
-                    optimized_routes[route][optimized_routes[route].index(dest)] = addresses[dest]
+                    self.optimized_routes[route][self.optimized_routes[route].index(dest)] = self.addresses[dest]
                 #Get directions
-                optimized_routes[route] = self.get_direction(optimized_routes[route])
+                self.optimized_routes[route] = self.get_direction(self.optimized_routes[route])
             else:
-                optimized_routes[route] = None
-        return optimized_routes    
+                self.optimized_routes[route] = None
+            
+        
+        return self.preprocess()
+    def preprocess(self):
+        """Cleans the response and only returns the required data, Overview Polyline.
+        """
+        processed_data = {}
+        for vehicle_idx in self.optimized_routes:
+            
+            if self.optimized_routes[vehicle_idx] != None:
+                processed_data[vehicle_idx] = {}
+                processed_data[vehicle_idx]["overview_polyline"] = self.optimized_routes[vehicle_idx][0]["overview_polyline"]["points"]
+                processed_data[vehicle_idx]["coordinates"] = self.extract_coordinates(self.optimized_routes[vehicle_idx])
+            else:
+                processed_data[vehicle_idx] = None
+        return processed_data
+                
+    def extract_coordinates(self, vehicle_direction):
+        processed = []
+        for coordinates in vehicle_direction[0]["legs"]:
+            processed.append(coordinates["end_location"])
+        return processed
+                
+                
+        
+                
+        
 
             
             
